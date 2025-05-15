2d970ba77af8fb67cc9da77df51ea76cab7b409c
Bhaskar Reddy Thavva <thavva.b@northeastern.edu>
2025-05-15 19:35:39 -0400

    updated common functionalities

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -3,6 +3,7 @@ import sys
 from src.logger import logging
 from src.custom_exception import CustomException
 import yaml
+import pandas as pd
 
 def read_yaml(file_path: str):
     try:
@@ -16,7 +17,14 @@ def read_yaml(file_path: str):
         raise CustomException(e, sys)
     
 
-    
+def load_data(path):
+    try:
+        logging.info("Loading data")
+        return pd.read_csv(path)
+    except Exception as e:
+        logging.error(f"Error loading the data {e}")
+        raise CustomException("Failed to load data" , e)
+
 
 
 
